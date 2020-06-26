fcc642e0c696fab6086d93395fc3cd3ee13d6300
misha2016 <mishabatsiashvili@yahoo.com>
2020-06-26 15:13:05 +0400

    Updates

---
+++ precache-manifest.b8f3cec70cc50b795647f24ddf30a2ec.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a99f24ca33a1056436aa0b4e0380b4c",
+    "url": "/ReactDiceGameHooks/index.html"
+  },
+  {
+    "revision": "820b5d6aaa007a8ac4d7",
+    "url": "/ReactDiceGameHooks/static/css/main.9e96e567.chunk.css"
+  },
+  {
+    "revision": "7b6c0195f057731d584d",
+    "url": "/ReactDiceGameHooks/static/js/2.091cee80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactDiceGameHooks/static/js/2.091cee80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820b5d6aaa007a8ac4d7",
+    "url": "/ReactDiceGameHooks/static/js/main.84ab6517.chunk.js"
+  },
+  {
+    "revision": "8a2ed46e0b5e34d94cdb",
+    "url": "/ReactDiceGameHooks/static/js/runtime-main.c2512374.js"
+  },
+  {
+    "revision": "3c3e64d64ec9756a2806aeb70fbc6fde",
+    "url": "/ReactDiceGameHooks/static/media/dice-1.3c3e64d6.png"
+  },
+  {
+    "revision": "2137b390e61006179e67a39cf205256b",
+    "url": "/ReactDiceGameHooks/static/media/dice-2.2137b390.png"
+  },
+  {
+    "revision": "c244a25e6a6db5dedb2f292b4dc69fbd",
+    "url": "/ReactDiceGameHooks/static/media/dice-3.c244a25e.png"
+  },
+  {
+    "revision": "f16153186f7ad72a11168fd9a4dff23c",
+    "url": "/ReactDiceGameHooks/static/media/dice-4.f1615318.png"
+  },
+  {
+    "revision": "36eb76a697b73106952e3f59da7e9e90",
+    "url": "/ReactDiceGameHooks/static/media/dice-5.36eb76a6.png"
+  },
+  {
+    "revision": "927fb6c975c727e227ff0a12e4880de1",
+    "url": "/ReactDiceGameHooks/static/media/dice-6.927fb6c9.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactDiceGameHooks/static/media/logo.5d5d9eef.svg"
+  }
+]);
